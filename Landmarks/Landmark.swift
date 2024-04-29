a95efa865dbd7794ca95c45f0e4778307015041b
EkeMinusYou <euonymuslke@gmail.com>
2024-04-29 16:52:58 +0900

    wip

--- Landmarks/Landmark.swift
+++ Landmarks/Landmark.swift
@@ -1,5 +1,6 @@
 import Foundation
 import SwiftUI
+import CoreLocation
 
 struct Landmark: Hashable, Codable {
   var id: Int
@@ -11,5 +12,17 @@ struct Landmark: Hashable, Codable {
   private var imageName: String
   var image: Image {
     Image(imageName)
+  }
+
+  private var coordinates: Coordinates
+  var locationCoordinate: CLLocationCoordinate2D {
+    CLLocationCoordinate2D(
+      latitude: coordinates.latitude,
+      longitude: coordinates.longitude)
+  }
+
+  struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
   }
 }
