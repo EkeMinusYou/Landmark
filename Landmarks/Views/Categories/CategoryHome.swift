c1ca25ed043de13b05744cb748ae88c1f054bbeb
EkeMinusYou <euonymuslke@gmail.com>
2024-05-07 04:00:33 +0900

    wip

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct CategoryHome: View {
+  @Environment(ModelData.self) var modelData
+
+  var body: some View {
+    NavigationSplitView {
+      List {
+        ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+          Text(key)
+        }
+      }
+      .navigationTitle("Featured")
+    } detail: {
+      Text("Detail")
+    }
+  }
+}
+
+#Preview {
+  CategoryHome()
+    .environment(ModelData())
+}
